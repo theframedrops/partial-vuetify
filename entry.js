7de97796361494bb4d1c3ee90b5bb07479b265d2
Corbin Crutchley <crutchcorn@gmail.com>
2021-09-16 20:05:14 -0700

    Tree-shake all non-used assets

--- entry.js
+++ entry.js
@@ -1,10 +1,31 @@
-import Vuetify, { VCard, VCardText, VCardTitle } from 'vuetify/lib'
+// THESE IMPORTS MUST BE FROM THESE EXACT LOCATIONS OTHERWISE
+// TREE SHAKING DOESN'T WORK
+import Vuetify from 'vuetify/lib/framework'
+import {VApp} from 'vuetify/lib/components/VApp'
+import {VCalendar} from 'vuetify/lib/components/VCalendar'
+import {VDialog} from 'vuetify/lib/components/VDialog'
+import {VImg} from 'vuetify/lib/components/VImg'
+import {
+    VCard, VCardActions,
+    VCardSubtitle,
+    VCardText, VCardTitle
+} from 'vuetify/lib/components/VCard'
+import {VBtn} from 'vuetify/lib/components/VBtn'
+import {VSnackbar} from 'vuetify/lib/components/VSnackbar'
 
 export default {
-  Vuetify,
-  components: {
-    VCard,
-    VCardText,
-    VCardTitle,
-  }
+    Vuetify,
+    components: {
+        VApp,
+        VCalendar,
+        VDialog,
+        VImg,
+        VCard,
+        VCardActions,
+        VCardSubtitle,
+        VCardText,
+        VCardTitle,
+        VBtn,
+        VSnackbar
+    }
 }
